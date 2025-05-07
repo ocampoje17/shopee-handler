9859919e66c829158f0335797d4c899e2aca4fe0
Nam <namqhong@gmail.com>
2025-05-08 06:50:59 +0700

    bo limit body

--- src/server.ts
+++ src/server.ts
@@ -19,8 +19,8 @@ const app: Express = express();
 app.set("trust proxy", true);
 
 // Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
 app.use(helmet());
 app.use(rateLimiter);
